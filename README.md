660fb8dac2269a91e8281ab9f960e88c6d0c3292
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2020-05-29 22:53:32 +0700

    add history menu

---
+++ README.md
@@ -0,0 +1,5 @@
+# rofi-menu
+
+This project contains various [Rofi](https://github.com/davatorium/rofi) menus (aka. modi).
+
+- rofi_history - Shows files recently opened and launches an appropriate application. Supports .txt (vim is executed within termite), .pdf (opened with zathura), videos (opened with VLC), and osp (opened with OpenShot).
---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { stdenv, makeWrapper, lib, gnused, jq }:
+      stdenv.mkDerivation {
+        pname = "rofi-menu";
+        version = "0.1.0.0";
+        src = ./.;
+
+        nativeBuildInputs = [ makeWrapper ];
+
+        installPhase = ''
+          install -D rofi-menu-history $out/bin/rofi-menu-history
+        '';
+
+        postFixup = ''
+          wrapProgram $out/bin/rofi-menu-history --prefix PATH : ${lib.makeBinPath [ gnused jq ]}
+        '';
+
+        meta = {
+          homepage = "https://github.com/emmanuelrosa/rofi-menu";
+          description = "Various rofi menus (aka. modi)";
+          license = stdenv.lib.licenses.mit;
+        };
+      };
+in
+  with pkgs; pkgs.callPackage f { inherit makeWrapper; }
